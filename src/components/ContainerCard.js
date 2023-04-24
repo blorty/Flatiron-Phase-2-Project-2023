6ca4c3b430669b1efb81b7e406b504ceef40e4fc
matt roche <matt.roche@gmail.com>
2023-04-24 14:32:43 -0500

    updated (rough draft?) ContainerCard.js

--- src/components/ContainerCard.js
+++ src/components/ContainerCard.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 
 function ContainerCard () {
-    return (
+    const [batmanData, setBatmanData] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/Media")
+        .then((res) => res.json())
+        .then((data) => setBatmanData(data));
+        }, []);
+    
+        const [isSeen, setIsSeen] = useState(false);
+    
+      const handleMarkAsSeen = () => {
+        setIsSeen(!isSeen);
+      }
+    
+      return (
         <div>
-        <h3>This is the ContainerCard component</h3>
-        <p>Some content for the container card</p>
-        </div>
-    );
-};
+          {batmanData.map((batItem) => (
+            <div key={batItem.id}>
+                <image src={batItem.image} alt={batItem.title}/>
+              <h3>{batItem.title}</h3>
+              <p>{batItem.year}</p>
+              <p>
+                <strong>{`Director: ${batItem.director}`}</strong>
+              </p>
+              <p>{`Starring ${batItem.actors}`}</p>
+              <button onClick={handleMarkAsSeen}>{isSeen ? "Already seen" : "Have not seen"}</button>
+              </div>
+          ))}
+          </div>
+      )
+    };
+    
 
 export default ContainerCard;
