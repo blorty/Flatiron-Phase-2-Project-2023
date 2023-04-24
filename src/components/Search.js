d233edb7f5a868e1cff7d152c47c508e20278d67
matt roche <matt.roche@gmail.com>
2023-04-24 13:50:48 -0500

    search for updated

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,42 @@
 import React from "react";
 
-function Search () {
+function Search({ movies, onSearch }) {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchCategory, setSearchCategory] = useState("type");
+  
+    const handleSearchTermChange = (event) => {
+      setSearchTerm(event.target.value);
+    };
+  
+    const handleSearchCategoryChange = (event) => {
+      setSearchCategory(event.target.value);
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      onSearch(searchTerm, searchCategory);
+    };
+  
     return (
-        <div>
-        <h2>This is the Search component</h2>
-        <input type="text" placeholder="Search..." />
-        <button type="button">Search</button>
-        </div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Search by:
+          <select value={searchCategory} onChange={handleSearchCategoryChange}>
+            <option value="type">Type</option>
+            <option value="title">Title</option>
+            <option value="director">Director</option>
+            <option value="year">Year</option>
+          </select>
+        </label>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
     );
-};
-
+    
 export default Search;
